1181312cc21f1a1c87a1bb0d77917ee2d10f429d
AlanCruz10 <exalan30@gmail.com>
2024-07-22 21:20:07 -0600

    [ADD] 2 main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = fastapi.FastAPI()
 
 @app.get("/")
 def read_root():
-    return fastapi.responses.JSONResponse(content={"message": "Hello, world!"})
+    return fastapi.responses.JSONResponse(content={"message": "Hello, world 2!"})
 
 
 if __name__ == "__main__":
